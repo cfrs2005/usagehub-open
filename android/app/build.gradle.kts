41bc8b00290232d8e7585cf9827c12cee9e70f3c
zhangqingyue <cfrs2005@gmail.com>
2026-09-03 22:23:47 -0700

    chore: prepare v0.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.usagehub.app"
         minSdk = 30
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "android.app.Instrumentation"
     }
